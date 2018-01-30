73b969d0ea06178ef04a97843793ef89eab5ad52
ktiwari2 <ktiwari2@users.noreply.github.com>
2018-01-29 21:40:00 -0500

    more update

--- InClassAssignment/InClass03/app/src/main/java/com/example/dimanor3/inclass03/MainActivity.java
+++ InClassAssignment/InClass03/app/src/main/java/com/example/dimanor3/inclass03/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         //setContentView (R.layout.activity_main);
-        setContentView (R.layout.activity_main);
+        setContentView (R.layout.display_activity);
         setTitle ("Main Activity");
 
         student = new Student ("", "", "", 0);
