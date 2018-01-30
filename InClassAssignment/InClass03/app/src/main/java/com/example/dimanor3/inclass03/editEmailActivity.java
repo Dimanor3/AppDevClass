3b47b3e6c68420de16e1f650786bcbe9b0e5ae62
ktiwari2 <ktiwari2@users.noreply.github.com>
2018-01-29 21:13:19 -0500

    got all edits

---
+++ InClassAssignment/InClass03/app/src/main/java/com/example/dimanor3/inclass03/editEmailActivity.java
@@ -0,0 +1,14 @@
+package com.example.dimanor3.inclass03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView (R.layout.editemail);
+    }
+}
