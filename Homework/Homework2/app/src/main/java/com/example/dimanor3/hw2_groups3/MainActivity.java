2345a56f1c0e711780003601963fb5dbc8316acb
brazavi <brazavi@uncc.edu>
2018-02-05 12:36:04 -0500

    New UPDATES WEEEEEEEgit add .!

--- Homework/Homework2/app/src/main/java/com/example/dimanor3/hw2_groups3/MainActivity.java
+++ Homework/Homework2/app/src/main/java/com/example/dimanor3/hw2_groups3/MainActivity.java
@@ -8,6 +8,9 @@ package com.example.dimanor3.hw2_groups3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView (R.layout.activity_main);
 
         setTitle ("View Tasks");
+    }
+
+    public void makeNewTask (View v) {
+        Log.d ("Test", "ADD NEW!!!");
+    }
+
+    public void gotoFirst (View v) {
+        Log.d ("Test", "FIRST!!!");
+    }
+
+    public void gotoPrevious (View v) {
+        Log.d ("Test", "PREVIOUS!!!");
+    }
+
+    public void edit (View v) {
+        Log.d ("Test", "EDIT!!!");
+    }
+
+    public void delete (View v) {
+        Log.d ("Test", "DELETED!!!");
+    }
+
+    public void gotoNext (View v) {
+        Log.d ("Test", "NEXT!!!");
+    }
+
+    public void gotoLast (View v) {
+        Log.d ("Test", "LAST!!!");
     }
 }
