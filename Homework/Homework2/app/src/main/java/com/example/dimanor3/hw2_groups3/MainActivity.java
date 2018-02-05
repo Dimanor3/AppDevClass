bf819349267d6a1cfc66f0e562ff91653ec8c299
brazavi <brazavi@uncc.edu>
2018-02-05 12:02:02 -0500

    Started HW2

---
+++ Homework/Homework2/app/src/main/java/com/example/dimanor3/hw2_groups3/MainActivity.java
@@ -0,0 +1,21 @@
+/*
+* Homework Assignment:  2
+* File Name:            MainActivity.java
+* Full Name:            Bijan Razavi, Kushal Tiwari
+* */
+
+package com.example.dimanor3.hw2_groups3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+
+        setTitle ("View Tasks");
+    }
+}
