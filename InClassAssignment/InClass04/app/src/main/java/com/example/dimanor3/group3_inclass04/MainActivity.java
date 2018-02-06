438c84857da5d711b1305a6d18b84b373023d1f8
brazavi <brazavi@uncc.edu>
2018-02-05 19:11:29 -0500

    Welcome Screen

--- InClassAssignment/InClass04/app/src/main/java/com/example/dimanor3/group3_inclass04/MainActivity.java
+++ InClassAssignment/InClass04/app/src/main/java/com/example/dimanor3/group3_inclass04/MainActivity.java
@@ -1,5 +1,13 @@
+/*
+* Assignment#:  InClass 03
+* File Name:    MainActivity.java
+* Full Name:    Bijan Razavi, Kushal Tiwari
+* */
+
 package com.example.dimanor3.group3_inclass04;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        setTitle ("Welcome!!!");
+
+        new Handler ().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent (this, PasswordGenerator.class);
+
+                startActivity (intent);
+            }
+        }, 3000);
     }
 }
