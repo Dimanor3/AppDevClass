a09e71808460c952c038e619b3436e15d6c814f5
Bijan Razavi <brazavi@uncc.edu>
2018-02-18 18:24:13 -0500

    hw3 init

---
+++ Homework/Homework3/Group#3_HW03/app/src/main/java/com/example/dimanor3/group3_hw03/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dimanor3.group3_hw03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate (savedInstanceState);
+		setContentView (R.layout.activity_main);
+	}
+}
